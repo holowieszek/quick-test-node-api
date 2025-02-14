dab4d7dc1cc4b14c30172727ee9955dc2fe4b241
Mateusz Holowieszko <holowieszko.m@gmail.com>
2025-02-15 00:45:35 +0100

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/hello', async (req, res) => {
+    res.status(200).json({ message: "Hello world" })
+})
+
+app.get('/hello2', async (req, res) => {
+    res.status(200).json({ message: "Hello world 2" })
+})
+
+app.listen(3000, () => console.log("Server running on port 3000"));
